462d7566374d0aafcf357c9d66215b7105765510
Irad Doron <irad.doron@gmail.com>
2023-05-13 02:29:38 +0300

    comment out icon import and component

--- src/pages/ComponentsTest/ComponentsTest.tsx
+++ src/pages/ComponentsTest/ComponentsTest.tsx
@@ -2,7 +2,7 @@ import Button from '@/shared/Button';
 import Chip from '@/shared/Chip';
 import Icon from '@/shared/Icon';
 import { Box, Typography, useTheme } from '@mui/material';
-import close from '@/assets/icons/close.svg';
+// import close from '@/assets/icons/close.svg';
 
 const ComponentsTest = () => {
 	const theme = useTheme();
@@ -72,7 +72,7 @@ const ComponentsTest = () => {
 				<Chip label='אוכל טעים רצח' />
 			</Box>
 
-			<Icon src={close} size='xl' alt='close' />
+			{/* <Icon src={close} size='xl' alt='close' /> */}
 		</Box>
 	);
 };
