3e3fc9ff6725b10b119088ff5254f3e2a31b8c49
Irad Doron <irad.doron@gmail.com>
2023-05-14 12:02:36 +0300

    add containers to splash page

--- src/pages/Splash/Splash.tsx
+++ src/pages/Splash/Splash.tsx
@@ -1,42 +1,22 @@
 import BgImage from '@/pages/Splash/components/BgImage';
+import ButtonsContainer from '@/pages/Splash/components/ButtonsContainer';
 import SubTitle from '@/pages/Splash/components/SubTitle';
 import Title from '@/pages/Splash/components/Title';
+import TitlesContainer from '@/pages/Splash/components/TitlesContainer';
 import Button from '@/shared/Button';
 import { Box } from '@mui/material';
 
 const Splash = () => {
 	return (
 		<Box>
-			<Box
-				sx={{
-					position: 'absolute',
-					top: '33%',
-					left: '50%',
-					transform: 'translate(-50%, -50%)',
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-				}}
-			>
+			<TitlesContainer>
 				<Title />
 				<SubTitle />
-			</Box>
-			<Box
-				sx={{
-					position: 'absolute',
-					top: '66%',
-					left: '50%',
-					transform: 'translate(-50%, -50%)',
-					display: 'flex',
-					flexDirection: 'column',
-					gap: '10px',
-					width: '100%',
-					alignItems: 'center',
-				}}
-			>
+			</TitlesContainer>
+			<ButtonsContainer>
 				<Button label='התחברות באמצעות גוגל' shape='pill' />
 				<Button label='התחברות באמצעות מייל' shape='pill' />
-			</Box>
+			</ButtonsContainer>
 
 			<BgImage />
 		</Box>
