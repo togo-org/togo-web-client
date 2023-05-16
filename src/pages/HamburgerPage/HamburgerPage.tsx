72fe7d4bf0d51de43dfdb4baf51a9dd85bad5f8b
DoovDevan <haleviamit@gmail.com>
2023-05-16 17:15:17 +0300

    Added global src

--- src/pages/HamburgerPage/HamburgerPage.tsx
+++ src/pages/HamburgerPage/HamburgerPage.tsx
@@ -1,11 +1,12 @@
 import PageContainer from "@/pages/HamburgerPage/components/PageContainer"
 import BgImage from "@/shared/BgImage"
+import IMAGES from "@/utils/constants/IMAGES"
 
 const HamburgerPage = () => {
     return (
         <PageContainer>
 
-            <BgImage />
+            <BgImage src={IMAGES.BgImage} alt='Background Image' />
         </PageContainer>
     )
 }
