4aef60d159e68192a954c68266e7cae7e23b81a3
pelegsch666 <peleg.mazuz@gmail.com>
2023-05-19 11:26:36 +0300

    last

--- src/pages/HomePage/components/HeaderContainer/HeaderContainer.tsx
+++ src/pages/HomePage/components/HeaderContainer/HeaderContainer.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import useStyles from './HeaderContainer.useStyles';
+
 interface HeaderContainerProps {
   children: React.ReactNode;
 }
