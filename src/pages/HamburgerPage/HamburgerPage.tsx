c7890e139e91dd5a44308a5428768ac89651eac6
pelegsch666 <peleg.mazuz@gmail.com>
2023-05-21 09:45:15 +0300

    jj

--- src/pages/HamburgerPage/HamburgerPage.tsx
+++ src/pages/HamburgerPage/HamburgerPage.tsx
@@ -1,14 +1,14 @@
-import PageContainer from "@/pages/HamburgerPage/components/PageContainer"
-import BgImage from "@/shared/BgImage"
-import IMAGES from "@/utils/constants/IMAGES"
+import PageContainer from '@/pages/HamburgerPage/components/PageContainer';
+import BgImage from '@/shared/BgImage';
+
+import IMAGES from '@/utils/constants/IMAGES';
 
 const HamburgerPage = () => {
-    return (
-        <PageContainer>
+  return (
+    <PageContainer>
+      <BgImage src={IMAGES.BgImage} alt='Background Image' />
+    </PageContainer>
+  );
+};
 
-            <BgImage src={IMAGES.BgImage} alt='Background Image' />
-        </PageContainer>
-    )
-}
-
-export default HamburgerPage
+export default HamburgerPage;
