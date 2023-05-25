b2e1961c7f8c921fedd152763e80ec47275a0b29
Irad Doron <irad.doron@gmail.com>
2023-05-25 09:07:32 +0300

    delete navbar from ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
-import Navbar from '@/shared/Navbar';
 import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
 import { RecoilRoot } from 'recoil';
-
 
 const App = dynamic(() => import('../App'), { ssr: false });
 
 const MyApp = (props: AppProps) => {
   return (
     <RecoilRoot>
-      <Navbar />
       <App {...props} />
     </RecoilRoot>
   );
