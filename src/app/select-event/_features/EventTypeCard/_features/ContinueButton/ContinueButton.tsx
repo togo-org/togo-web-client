8e51b35f2eb2f475d0de302991dfaf57f79a991b
Irad Doron <irad.doron@gmail.com>
2023-06-05 20:18:58 +0300

    fix navigation bugs

--- src/app/select-event/_features/EventTypeCard/_features/ContinueButton/ContinueButton.tsx
+++ src/app/select-event/_features/EventTypeCard/_features/ContinueButton/ContinueButton.tsx
@@ -5,14 +5,17 @@ import { currentSelectEventTabState } from '@/store';
 import React from 'react';
 import { useSetRecoilState } from 'recoil';
 import useStyles from './ContinueButton.useStyles';
+import { useRouter } from 'next/navigation';
 
 const ContinueButton = () => {
-  const setSelectedTab = useSetRecoilState(currentSelectEventTabState);
+  const router = useRouter();
   const styles = useStyles();
+  const setSelectedTab = useSetRecoilState(currentSelectEventTabState);
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.stopPropagation();
-    setSelectedTab(1);
+    router.push('/select-event/how-much');
+    setSelectedTab('how-much');
   };
 
   return (
--- src/app/select-event/how-much/_features/ContinueButton/ContinueButton.tsx
+++ src/app/select-event/how-much/_features/ContinueButton/ContinueButton.tsx
@@ -4,12 +4,15 @@ import Button from '@/features/shared/Button';
 import { currentSelectEventTabState } from '@/store';
 import { useSetRecoilState } from 'recoil';
 import useStyles from './ContinueButton.useStyles';
+import { useRouter } from 'next/navigation';
 
 const ContinueButton = () => {
   const setSelectedTab = useSetRecoilState(currentSelectEventTabState);
+  const router = useRouter();
   const styles = useStyles();
   const handleClick = () => {
-    setSelectedTab(2);
+    setSelectedTab('when');
+    router.push('/select-event/when');
   };
   return (
     <Button label='המשך' shape='pill' sx={styles.root} onClick={handleClick} />
